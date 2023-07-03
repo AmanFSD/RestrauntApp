3887920dfdf24912997f954a890a44fbcfee231b
aman <aman0486@gmail.com>
2023-07-03 06:57:57 +0530

    README.md

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Restaurant restaurant = new Restaurant();
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("1. Add customer");
+            System.out.println("2. Serve customer");
+            System.out.println("3. Find customer");
+            System.out.println("4. Display queue");
+            System.out.println("5. Exit");
+            System.out.print("Enter choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();  // Consume newline left-over
+
+            if (choice == 1) {
+                System.out.print("Enter customer ID: ");
+                String id = scanner.nextLine();
+                System.out.print("Enter customer name: ");
+                String name = scanner.nextLine();
+                Customer customer = new Customer(id, name);
+                restaurant.addCustomer(customer);
+            } else if (choice == 2) {
+                Customer customer = restaurant.serveCustomer();
+                if (customer != null) {
+                    System.out.println("Serving customer: " + customer);
+                } else {
+                    System.out.println("No customers to serve.");
+                }
+            } else if (choice == 3) {
+                System.out.print("Enter customer ID: ");
+                String id = scanner.nextLine();
+                Customer customer = restaurant.findCustomer(id);
+                if (customer != null) {
+                    System.out.println("Found customer: " + customer);
+                } else {
+                    System.out.println("Customer not found.");
+                }
+            } else if (choice == 4) {
+                restaurant.displayQueue();
+            } else if (choice == 5) {
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
